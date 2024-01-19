c94a73ba4b8540dbe53a27a17a9e93b3723ed18d
pechaut78 <pe_chaut@hotmail.com>
2024-01-19 11:10:22 +0100

    Correction of errors in corpus

--- 1-train/train-mistral.sh
+++ 1-train/train-mistral.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --model_name="mistralai/Mistral-7B-Instruct-v0.1" --new_model="StarkWizard/Mistral-7b-instruct-cairo-PEFT" --lr=2e-5   q_proj k_proj v_proj o_proj gate_proj --epochs=10
+python train.py --model_name="mistralai/Mistral-7B-Instruct-v0.2" --new_model="StarkWizard/Mistral-7b-instruct-cairo-PEFT" --lr=2e-5   q_proj k_proj v_proj o_proj gate_proj --epochs=30
