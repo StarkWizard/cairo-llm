ec9c980d2a5f8a23ab51bc7ee2899081c1e6bcd5
pechaut78 <pe_chaut@hotmail.com>
2024-01-21 14:25:00 +0100

    Better corpus using paraphrase

--- 1-train/train-mistral.sh
+++ 1-train/train-mistral.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --model_name="mistralai/Mistral-7B-Instruct-v0.2" --new_model="StarkWizard/Mistral-7b-instruct-cairo-PEFT" --lr=2e-5   q_proj k_proj v_proj o_proj gate_proj --epochs=3
+python train.py --model_name="mistralai/Mistral-7B-Instruct-v0.2" --new_model="StarkWizard/Mistral-7b-instruct-cairo-PEFT" --lr=2e-5   q_proj k_proj v_proj o_proj gate_proj --window=256 --max_seq_length=1024 --epochs=10 --wandb_project="Mistral"
